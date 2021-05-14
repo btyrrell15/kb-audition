267f59dc02e0229e971ce987c8a5d695a40857d2
btyrrell15 <51797754+btyrrell15@users.noreply.github.com>
2021-05-13 21:06:42 -0600

    Update keys.js

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "",
-    secret: ""
+    mongoURI: "mongodb+srv://pogi:TangoMango!1@climate.obv4z.mongodb.net/kbaudition?retryWrites=true&w=majority",
+    secret: "keys"
 }
